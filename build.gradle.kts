c9a3c8d662ac65ae234316102e8ff229bbe67931
BBee <atsushi.kokubun.47@gmail.com>
2024-06-20 05:33:57 +0900

    Upgrade AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     val hiltVersion = "2.49"
-    id("com.android.application") version "8.1.4" apply false
+    id("com.android.application") version "8.4.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
     id("com.google.dagger.hilt.android") version hiltVersion apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
