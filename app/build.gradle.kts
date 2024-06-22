b86c3a53725be2c3ff7e726d0a6188865aad1b63
BBee <atsushi.kokubun.47@gmail.com>
2024-06-22 09:29:47 +0900

    Update version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "jp.developer.bbee.englishmemory"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.0.0"
+        versionCode = 4
+        versionName = "1.0.1"
 
         val baseUrl = getProp("AWS_BASE_URL", "baseUrl", properties)
         buildConfigField("String", "BASE_URL", "\"$baseUrl\"")
